2c539929542ecb18d35ed788c304a6333aa18157
Mustafa <mustafa1596315@gmail.com>
2024-06-28 02:45:18 +0300

    Errors in Fetch API and Styling the Product List

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { fetcher,getCategories,getProducts } from './fetcher';
 import Category from './components/Category';
+import CategoryProduct from './db/categoryProduct';
 
 function App() {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState({errorMessage: '',data:[]});
+  const [products, setProducts] = useState({errorMessage: '',data:[]});
 
-  useEffect(() => {
-    fetch("http://localhost:3001/categories")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setCategories(data);
-      })
-      .catch(error => {
-        console.error("Error fetching data: ", error);
-      });
+useEffect(() => {
+  const fetchData =async ()=>{
+    const responseObject =await getCategories();
+    setCategories(responseObject);
+  }
+  fetchData();
   }, []);
 
   const handleCategoryClick=id =>{
-    fetch("http://localhost:3001/products?catId=" + id)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setProducts(data);
-    })
+    const fetchData =async ()=>{
+      const responseObject =await getProducts(id);
+      setProducts(responseObject);
+    }
+    fetchData();
   }
 
   const renderCategories =()=>{
-     return categories.map(c=>
+     return categories.data.map(c=>
        <Category key={c.id} id={c.id} title={c.title} onCategoryClick={()=> handleCategoryClick(c.id)}/>
      );
   }
 
   const renderProducts=()=>{
-    return products.map(p=>
-      <div>{p.title}</div>
-    )
+    return products.data.map(p=>
+      <CategoryProduct{...p}>{p.title}</CategoryProduct>
+    );
   }
 
   return (
@@ -45,15 +42,17 @@ function App() {
     <header>My Store </header>
     <section>
       <nav>
+        {categories.errorMessage && <div>Error:{categories.errorMessage}</div>}
         {
-         categories && renderCategories()
+         categories.data && renderCategories()
         }
       </nav>
 
-      <article>
+      <main>
         <h1>Products</h1>
-        { products && renderProducts()}
-      </article>
+        {products.errorMessage && <div>Error:{products.errorMessage}</div>}
+        { products.data && renderProducts()}
+      </main>
     </section>
 
     <footer>
